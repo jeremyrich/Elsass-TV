72d9644ebea856c5bf4733fc6845b0988ea1051a
qdufrois <quentin.dufrois@gmail.com>
2019-02-05 14:22:43 +0100

    Addition of the Movie model

--- Elsasstv/movies/models.py
+++ Elsasstv/movies/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+class Movie(models.Model):
+
+    id = models.IntegerField(primary_key=True)
+    imdb_id = models.CharField(max_length=45)
+    original_title = models.CharField(max_length=200)
+    title = models.CharField(max_length=200)  
+    tagline = models.CharField(max_length=200)  
+    overview = models.TextField()
+    release_date = models.DateTimeField()
+    status = models.CharField(max_length=45)
+    runtime = models.IntegerField(null=True)
+    popularity = models.FloatField(null=True)
+    original_langage = models.CharField(max_length=200)
+    poster_path = models.CharField(max_length=200)
+    backdrop_path = models.CharField(max_length=200)    
+    budget = models.BigIntegerField(null=True)
+    revenue = models.BigIntegerField(null=True)   
+    vote_average = models.FloatField(null=True)
+    vote_count = models.IntegerField(null=True)
+    
+    
