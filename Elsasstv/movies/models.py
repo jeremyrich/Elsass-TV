b70dd35409d1d30b6de67e387b55a09704cb50af
qdufrois <46813122+qdufrois@users.noreply.github.com>
2019-02-05 14:33:34 +0100

    Update models.py

--- Elsasstv/movies/models.py
+++ Elsasstv/movies/models.py
@@ -5,12 +5,12 @@ class Person(models.Model):
     id = models.IntegerField(primary_key=True)
     imdb_id = models.CharField(max_length=45)
     name = models.CharField(max_length=200)
-    gender = models.IntegerField
-    birthday = models.DateTimeField
+    gender = models.IntegerField()
+    birthday = models.DateTimeField()
     deathday = models.DateTimeField(null=True)
     place_of_birth = models.CharField(max_length=200)
-    known_for_department = models.Charfield(max_length=45)
-    biography = models.TextField 
-    Popularity = models.FloatField 
+    known_for_department = models.CharField(max_length=45)
+    biography = models.TextField() 
+    Popularity = models.FloatField() 
     profile_path = models.CharField(max_length=200)
     
