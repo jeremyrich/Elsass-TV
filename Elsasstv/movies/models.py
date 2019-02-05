734042f97b015d2a36ea1e8c1f084d351973bbd8
Florence Schwartz <schflo@gmail.com>
2019-02-05 14:29:24 +0100

    finished person model

--- Elsasstv/movies/models.py
+++ Elsasstv/movies/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Person(models.Model):
+    id = models.IntegerField(primary_key=True)
+    imdb_id = models.CharField(max_length=45)
+    name = models.CharField(max_length=200)
+    gender = models.IntegerField
+    birthday = models.DateTimeField
+    deathday = models.DateTimeField(null=True)
+    place_of_birth = models.CharField(max_length=200)
+    known_for_department = models.Charfield(max_length=45)
+    biography = models.TextField 
+    Popularity = models.FloatField 
+    profile_path = models.CharField(max_length=200)
+    
