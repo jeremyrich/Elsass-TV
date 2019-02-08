7717acdddd7fc4f4399bf20c288344f6c31ceeb6
qdufrois <quentin.dufrois@gmail.com>
2019-02-08 09:28:59 +0100

    Home page displays the 100th more popular movies

--- Elsasstv/movies/tests.py
+++ Elsasstv/movies/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+
+class MovieTest(TestCase):
+
+    def test_home_view(self):
+        """Tests if the request to home page sends a response 200"""        
+        response = self.client.get(reverse('movies:home'))
+        self.assertEqual(response.status_code, 200)
--- Elsasstv/movies/views.py
+++ Elsasstv/movies/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from movies.models import Movie
 
 
 def home(request):
-
+    """View rendering the 100th more popular movies"""
+    popular_movies = Movie.objects.order_by('-popularity')[:100]    
     return render(request, 'movies/home.html', locals())
