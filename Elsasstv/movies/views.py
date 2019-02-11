f6fb39f4566d7c8d1713862c06846d09101385c9
jeremyrich <jeremyrich@free.fr>
2019-02-11 15:44:17 +0100

    Added username if logged to movie home and detail page

--- Elsasstv/movies/views.py
+++ Elsasstv/movies/views.py
@@ -2,16 +2,18 @@
 from django.shortcuts import render, get_object_or_404
 from movies.models import Movie, Person
 from django.contrib.auth import login, authenticate, logout
-
+from django.contrib.auth.decorators import login_required
 
 def home(request):
     """View rendering the 100th more popular movies"""
+    username = request.user.username
     popular_movies = Movie.objects.order_by('-popularity')[:100]
     first_popular = Movie.objects.order_by('-popularity')[0]
     logout(request)
     return render(request, 'movies/home.html', locals())
 
 def detail(request, movie_id):
+    username = request.user.username
     movie = get_object_or_404(Movie, pk=movie_id)
     person = Person.objects.filter(profile_path__isnull=False)[:3]
     similar_movies = Movie.objects.order_by('-popularity')[:4]
