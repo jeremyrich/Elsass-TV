71166d1ebc99272907b1e0cee85a8e7621bf8f15
jeremyrich <jeremyrich@free.fr>
2019-02-11 15:20:41 +0100

    Added login form in modal + logout link

--- Elsasstv/Elsasstv/views.py
+++ Elsasstv/Elsasstv/views.py
@@ -1,12 +1,32 @@
 from django.shortcuts import render, redirect
 from movies.models import Movie
-# from github.forms import LoginForm
-
+from django.contrib.auth import login, authenticate, logout
+from .forms import UserForm
 
 def home(request):
 
     popular_movies = Movie.objects.order_by('-popularity')[:100] 
-    
+
+    # Beginning of login_user_form view
+    error = False
+
+    if request.method == 'POST':
+        login_user = UserForm(request.POST)
+
+        if login_user.is_valid():
+            username = login_user.cleaned_data['username']
+            password = login_user.cleaned_data['password']
+            user = authenticate(username=username, password=password) 
+
+            if user:  
+                login(request, user)  
+                return redirect('movies:home')
+            else: 
+                error = True                    
+
+    else:
+        login_user = UserForm() 
+    # end of login_user_form view
 
     return render(request, 'home.html', locals())
 
--- Elsasstv/movies/views.py
+++ Elsasstv/movies/views.py
@@ -1,17 +1,19 @@
 
 from django.shortcuts import render, get_object_or_404
 from movies.models import Movie, Person
+from django.contrib.auth import login, authenticate, logout
 
 
 def home(request):
     """View rendering the 100th more popular movies"""
     popular_movies = Movie.objects.order_by('-popularity')[:100]
     first_popular = Movie.objects.order_by('-popularity')[0]
+    logout(request)
     return render(request, 'movies/home.html', locals())
 
 def detail(request, movie_id):
     movie = get_object_or_404(Movie, pk=movie_id)
     person = Person.objects.filter(profile_path__isnull=False)[:3]
     similar_movies = Movie.objects.order_by('-popularity')[:4]
-
+    logout(request)
     return render (request, 'movies/movie-detail.html', locals())
